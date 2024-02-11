8ed80dc93dcc8ae619ce2bdb815be29600382a38
Alexis-NM <alexis.na.me@gmail.com>
2024-02-11 19:44:38 +0100

    styling pages and links on frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,26 @@
+import Title from "../components/Title";
+
+import "../styles/pages/Login.scss";
+import onButton from "../assets/icons/on_button.svg";
+
 function Login() {
   return (
     <div>
-      <h1>Login</h1>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="button">Login</button>
+      <Title />
+      <p className="connexion-title">Connexion</p>
+      <form className="form-area">
+        <div className="input-container">
+          <p className="text-form">Identifiant :</p>
+          <input className="form-input" type="text" placeholder="" />
+        </div>
+        <div className="input-container">
+          <p className="text-form">Mot de passe :</p>
+          <input className="form-input" type="password" placeholder="" />
+        </div>
+        <button type="button" className="submit-button">
+          Je me connecte
+          <img src={onButton} alt="on_button" className="button-icon" />
+        </button>
       </form>
     </div>
   );
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,48 @@
+import Title from "../components/Title";
+
+import "../styles/pages/Register.scss";
+import plusButton from "../assets/icons/white_plus.svg";
+import lamp from "../assets/logos/lamp.svg";
+
 function Register() {
   return (
     <div>
-      <h1>Register</h1>
+      <Title />
+      <p className="connexion-title-register">Créer mon compte</p>
+      <div className="account-phrase-register">
+        <img className="lamp-logo" src={lamp} alt="Logo d'une ampoule" />
+        <p>Partagez de nouvelles astuces !</p>
+      </div>
+      <form className="form-area-register">
+        <div className="input-container-register">
+          <p className="text-form-register">Identifiant :</p>
+          <input className="form-input-register" type="text" placeholder="" />
+        </div>
+        <div className="input-container-register">
+          <p className="text-form-register">Mot de passe :</p>
+          <input
+            className="form-input-register"
+            type="password"
+            placeholder=""
+          />
+        </div>
+        <div className="input-container-register">
+          <p className="text-form-register">Confirmer le mot de passe :</p>
+          <input
+            className="form-input-register"
+            type="password"
+            placeholder=""
+          />
+        </div>
+        <button type="button" className="submit-button">
+          Je créer mon compte
+          <img
+            src={plusButton}
+            alt="on_button"
+            className="button-icon-register"
+          />
+        </button>
+      </form>
     </div>
   );
 }
